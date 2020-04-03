95e139b496b712ff0c6c910e789d926ec9d7dc44
Yicheng YANG <felix.yang.yicheng@gmail.com>
2020-04-03 14:44:09 +0200

    3

--- SiteBugs.BD/DAL/BugsContext.cs
+++ SiteBugs.BD/DAL/BugsContext.cs
@@ -34,10 +34,44 @@ namespace SiteBugs.BD.DAL
                 {
                     Bug bug = new Bug();
                     bug.identifiant = reader.GetInt32("identifiant");
+                    bug.Titre = reader.GetString("Titre");
+                    bug.date = reader.GetDateTime("date");
+                    bug.bloquant = reader.GetBoolean("bloquant");
+                    bug.identifiantSeverite = reader.GetInt32("identifiantSeverite");
+
+                    bugs.Add(bug);
                 }
 
             }
+            return bugs;
+        }
 
+        public Bug Get(int identifiant) 
+        {
+            Bug bug = null;
+            using (MySqlConnection connection = new MySqlConnection())
+            {
+                connection.ConnectionString = connectionString;
+                connection.Open();
+
+                MySqlCommand command = connection.CreateCommand();
+                command.CommandText = "SELECT identifiant, Titre, date, bloquant, identifiantSeverite FROM Bugs WHERE identifiant=@identifiant;";
+
+                command.Parameters.AddWithValue("identifiant", identifiant);
+
+                MySqlDataReader reader = command.ExecuteReader();
+
+                while (reader.Read()) 
+                {
+                    bug = new Bug();
+                    bug.identifiant = reader.GetInt32("identifiant");
+                    bug.Titre = reader.GetString("Titre");
+                    bug.date = reader.GetDateTime("date");
+                    bug.bloquant = reader.GetBoolean("bloquant");
+                    bug.identifiantSeverite = reader.GetInt32("identifiantSeverite");
+                }
+            }
         }
     }
+
 }
